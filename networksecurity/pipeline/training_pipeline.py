acad11f42c26b73827c662521238bb2564b3698e
sameer-2642 <sameervishwamitre2642@gmail.com>
2026-08-30 00:27:00 +0530

    add S3 sync module and training pipeline with cloud integration

--- networksecurity/pipeline/training_pipeline.py
+++ networksecurity/pipeline/training_pipeline.py
@@ -1,4 +1,3 @@
-from networksecurity.constants.training_pipeline import TRAINING_BUCKET_NAME
 import os
 import sys
 
@@ -24,20 +23,28 @@ from networksecurity.entity.artifact_entity import (
     DataTransformationArtifact,
     ModelTrainerArtifact,
 )
+
+from networksecurity.constants.training_pipeline import TRAINING_BUCKET_NAME
+from networksecurity.cloud.s3_syncer import S3Sync
+from networksecurity.constants.training_pipeline import SAVED_MODEL_DIR
 import sys
+
 
 class TrainingPipeline:
     def __init__(self):
-        self.training_pipeline = TrainingPipeline()
-    
+        self.training_pipeline_config=TrainingPipelineConfig()
+        self.s3_sync = S3Sync()
+        
+
     def start_data_ingestion(self):
         try:
-            self.data_ingestion_config = DataIngestionConfig(training_pipeline_config=self.training_pipeline_config)
-            logging.info("start data Ingestion")
-            data_ingestion = DataIngestion(data_ingestion_config=self.data_ingestion_config)
-            data_ingestion_artifact = data_ingestion.initiate_data_ingestion()
-            logging.info("Data ingestion completed and artifact:{data_ingestion_artifact}")
+            self.data_ingestion_config=DataIngestionConfig(training_pipeline_config=self.training_pipeline_config)
+            logging.info("Start data Ingestion")
+            data_ingestion=DataIngestion(data_ingestion_config=self.data_ingestion_config)
+            data_ingestion_artifact=data_ingestion.initiate_data_ingestion()
+            logging.info(f"Data Ingestion completed and artifact: {data_ingestion_artifact}")
             return data_ingestion_artifact
+        
         except Exception as e:
             raise NetworkSecurityException(e,sys)
         
@@ -50,7 +57,7 @@ class TrainingPipeline:
             return data_validation_artifact
         except Exception as e:
             raise NetworkSecurityException(e,sys)
-    
+        
     def start_data_transformation(self,data_validation_artifact:DataValidationArtifact):
         try:
             data_transformation_config = DataTransformationConfig(training_pipeline_config=self.training_pipeline_config)
@@ -112,3 +119,5 @@ class TrainingPipeline:
             return model_trainer_artifact
         except Exception as e:
             raise NetworkSecurityException(e,sys)
+        
+    
